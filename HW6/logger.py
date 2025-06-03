2a294361a3ef556be1a6c2d2860f61c6299a9a67
zhbforum <lyosha.minakoff@gmail.com>
2025-06-03 17:22:00 +0200

    changes

--- HW6/logger.py
+++ HW6/logger.py
@@ -1,16 +1,16 @@
 import logging
-import os  
+import os
 
 from constants import LOGGER_NAME, LOG_FILE_PATH, LOG_FORMAT, DEFAULT_ENCODING
 
 
 def init_logger() -> logging.Logger:
+    os.makedirs(os.path.dirname(LOG_FILE_PATH), exist_ok=True)
+
     logger = logging.getLogger(LOGGER_NAME)
     logger.setLevel(logging.INFO)
 
     if not logger.handlers:
-        os.makedirs(os.path.dirname(LOG_FILE_PATH), exist_ok=True)
-
         handler = logging.FileHandler(LOG_FILE_PATH, encoding=DEFAULT_ENCODING)
         formatter = logging.Formatter(LOG_FORMAT)
         handler.setFormatter(formatter)
